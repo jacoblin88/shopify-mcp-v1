7558633883c794fd4bb50515ee1d7b5798dbc110
jacoblin88 <jacoblin88@gmail.com>
2026-04-07 11:23:52 +0800

    Add OAuth authentication

--- server.py
+++ server.py
@@ -64,7 +64,7 @@ logger = logging.getLogger("shopify_mcp")
 PORT          = int(os.environ.get("PORT", "8000"))
 MCP_TRANSPORT = os.environ.get("MCP_TRANSPORT", "streamable-http")
 
-mcp = FastMCP("shopify_mcp", host="0.0.0.0", port=PORT, json_response=True,auth=auth_provider)
+mcp = FastMCP("shopify_mcp",port=PORT, json_response=True,auth=auth_provider)
 
 
 # ---------------------------------------------------------------------------
